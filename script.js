7ffd02f8e4d8b67c51afd0fd92f31972a90348cb
Danilo-Martins1 <danilo.martins1819@gmail.com>
2023-06-28 23:03:41 -0300

    Simples gerador de senha

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let sliderelement = document.querySelector("#slider");
+let buttonelement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let passoword = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789!@#$%-=*+";
+let novasenha = "";
+
+sizePassword.innerHTML = sliderelement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+    
+    for(let i = 0, n = charset.length; i < sliderelement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random()* n));
+    }
+    
+    containerPassword.classList.remove("hide");
+    passoword.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
